f40bdb146e8aaf4c4983e3f18ead9c49050ccfbe
bainingjie <ningjie20081006@gmail.com>
2022-11-30 09:42:22 +0900

    exp2

--- run_kgat.py
+++ run_kgat.py
@@ -6,4 +6,4 @@ parameter_dict = {
 #   'neg_sampling': None,
 }
 config_file_list = ['kgat.yaml']
-run_recbole(model='KGAT', dataset='ml-1m', config_file_list=config_file_list, config_dict=parameter_dict)
+run_recbole(model='KGAT', dataset='amazon-books', config_file_list=config_file_list, config_dict=parameter_dict)
